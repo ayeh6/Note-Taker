23b319187562cacc63e2f85ac7da76723cf82b0c
Andrew Yeh <ayeh2317@gmail.com>
2022-07-24 16:14:46 -0700

    used wildcard for index.html route

--- routes/publicRoutes/index.js
+++ routes/publicRoutes/index.js
@@ -1,14 +1,12 @@
 const router = require('express').Router();
 const path = require('path');
 
-router.route('/')
-    .get((req,res) => {
-        res.sendFile(path.join(__dirname, './../../public/index.html'));
-    });
-
-router.route('/notes')
-    .get((req,res) => {
+router.route('/notes').get((req,res) => {
         res.sendFile(path.join(__dirname, './../../public/notes.html'));
     });
+
+router.route('*').get((req,res) => {
+    res.sendFile(path.join(__dirname, './../../public/index.html'));
+});
 
 module.exports = router;
